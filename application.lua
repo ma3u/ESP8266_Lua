2e60697b3e37f98ba20f85a3c48f3a09dafc51e2
Antonio P. P. Almeida <appa@couperin.couperin.relayr.no-ip>
2016-06-02 17:28:49 +0200

    * Style fixes mostly. Minor logic tweaks.

--- application.lua
+++ application.lua
@@ -1,79 +1,154 @@
----  Load WiFi and relayr-API modules.
+
+-- application.lua ---
+
+-- Copyright (C) 2016 António P. P. Almeida <appa@perusio.net>
+
+-- Author: António P. P. Almeida <appa@perusio.net>
+
+-- Permission is hereby granted, free of charge, to any person obtaining a
+-- copy of this software and associated documentation files (the "Software"),
+-- to deal in the Software without restriction, including without limitation
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense,
+-- and/or sell copies of the Software, and to permit persons to whom the
+-- Software is furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+
+-- Except as contained in this notice, the name(s) of the above copyright
+-- holders shall not be used in advertising or otherwise to promote the sale,
+-- use or other dealings in this Software without prior written authorization.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+-- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+-- DEALINGS IN THE SOFTWARE.
+
+--  Load WiFi and relayr-API modules.
 local wifi_setup = require 'wifi-setup'
 local relayr = require 'relayr-API'
 
----  Your WiFi SSID and password.
-local WIFI_config = { ssid = 'relayr24', psk = 'wearsimaspants' }
----  MQTT credentials from the developer dashboard.
-local MQTT_config = {
-    deviceID = '03097f9c-95b1-4f24-86bf-2dd700555438',
-    user = '03097f9c-95b1-4f24-86bf-2dd700555438',
-    password = 'PkbYdOlz32aP',
-    clientID = 'TAwl/nJWxTySGvy3XAFVUOA',
-    topic = '/v1/03097f9c-95b1-4f24-86bf-2dd700555438/',
-    server = 'mqtt.relayr.io'
+-- Local definitions.
+local format = string.format
+
+--[[
+  **************************************************
+  **** Configuration of the module starts here. ****
+  **************************************************
+]]--
+
+--  Your WiFi SSID and password/psk.
+local wifi_config = {
+  ssid = 'relayrGuest',
+  psk = 'ChangingTheWorldwithIoT!'
 }
+---  MQTT credentials you get from the developer dashboard.
+local mqtt_config = {
+  user = '03097f9c-95b1-4f24-86bf-2dd700555438',
+  -- User password for basic authorization.
+  password = 'PkbYdOlz32aP',
+  -- This is just a convenience that allows you to identify the
+  -- client on the MQTT bro ker. It can be anything you choose.
+  clientID = 'TAwl/nJWxTySGvy3XAFVUOA',
+  -- MQTT topic for the device that you're connected to.
+  topic = '/v1/03097f9c-95b1-4f24-86bf-2dd700555438',
+}
+--[[
+  **************************************************
+  **** Configuration of the module ends here.   ****
+  **************************************************
+]]--
+
+--[[
+  **************************************************
+  ************ User defined functions. *************
+  **************************************************
+]]--
 
 --- Number of the GPIO pin used as a digital output.
-local outputPin = 0
+local output_pin = 0
 
---- Callback triggered by received data from 'cmd' and 'config' topics.
+--- Callback triggered by received data from 'cmd' and 'config'
+--  topics. See below for setup function.
+-- @param table data
+--   Data received via MQTT on both topics.
+-- @return nothing
+--   Side effects only.
 local function received_data(data)
-    -- Print the name and value in received JSON message.
-    print(string.format('Received: (name: %s, value: %s)', data.name, tostring(data.value)))
-    -- Process the messages with 'Output' name.
-    if data.name == 'Output' then
-        if data.value then
-            gpio.write(outputPin, gpio.HIGH)
-        else
-            gpio.write(outputPin, gpio.LOW)
-        end
-    end
-    -- Process the messages with name 'Frequency'.
-    if data.name == 'Frequency' then
-        -- Update the alarm interval
-        -- for sending data to relayr Cloud.
-        tmr.interval(2, data.value)
-    end
-end
-
---- Function for sending data to relayr Cloud.
-local function send_data()
-    -- Read the ADC input.
-    local reading = adc.read(0)
-    -- Send the data to relayr Cloud.
-    relayr.send({ meaning = 'ADC', value = reading })
-end
-
---- Connect to ralyr Cloud and setup GPIOs.
-local function setup()
-    -- Setup GPIO as an output.
-    gpio.mode(outputPin, gpio.OUTPUT)
-    -- Register the function (callback) in which you
-    -- whish to process incoming data (commands).
-    relayr.register_data_listener(received_data)
-    -- Connect to relayr Cloud.
-    relayr.connect(MQTT_config,
-        -- Callback when the connection is established.
-        function()
-            -- Trigger 'send_data' function every 2000ms
-            tmr.alarm(2,2000,1,send_data)
-        end)
-end
-
---- Setup WiFi and connect to it.
-wifi_setup.start(WIFI_config)
--- Check if the IP is already assigned.
-local function wifi_wait_ip()
-    if not wifi.sta.getip() then
-        print('IP address unassigned: waiting for it.')
+  -- Print the name and value in received JSON message.
+  print(format('Received: (name: %s, value: %s).',
+               data.name,
+               tostring(data.value)))
+  -- Process the messages with 'Output' name.
+  if data.name == 'Output' then
+    if data.value then
+      gpio.write(output_pin, gpio.HIGH)
     else
-        -- IP assigned, unregister the timer and print the IP address.
-        tmr.unregister(1)
-        print('Assigned IP address: ' .. wifi.sta.getip())
-        -- Execute the 'setup' function.
-        setup()
+      gpio.write(output_pin, gpio.LOW)
     end
+  end
+  -- Process the messages with name 'Frequency'.
+  if data.name == 'Frequency' then
+    -- Update the alarm interval
+    -- for sending data to relayr Cloud.
+    tmr.interval(2, data.value)
+  end
 end
+
+--- Wrapper for sending data to the relayr Cloud.
+-- @return nothing.
+--   Side effects only.
+local function send_data()
+  -- Read the ADC input.
+  local reading = adc.read(0)
+  -- Send the data to relayr Cloud.
+  relayr.send({ meaning = 'ADC input', value = reading })
+end
+
+--- Setup whatever you need in order to send
+--  data and connect to the relayr cloud to
+--  send it.
+-- @return nothing.
+--  Side effects only.
+local function setup()
+  -- Setup GPIO as an output.
+  gpio.mode(output_pin, gpio.OUTPUT)
+  -- Register the function (callback) in which you
+  -- whish to process incoming data (commands).
+  relayr.register_data_listener(received_data)
+  -- Connect to relayr Cloud.
+  relayr.connect(
+    -- Pass the MQTT configuration.
+    mqtt_config,
+    -- Callback when the connection is established.
+    -- Trigger the 'send_data' function every 2000 ms.
+    function() tmr.alarm(2, 2000, 1, send_data) end
+  )
+end
+
+-- Setup WiFi and connect to it.
+wifi_setup.start(wifi_config)
+
+--- Callback that checks the WiFi link
+-- is established and prints the IP address
+-- when done.
+--
+-- @return nothing.
+--  Side effects only.
+local function wifi_wait_ip()
+  if not wifi.sta.getip() then
+    print('IP address unassigned: waiting for it.')
+  else
+    -- IP assigned, unregister the timer and print the IP address.
+    tmr.unregister(1)
+    print(format('Assigned IP address: %s.', wifi.sta.getip()))
+    -- Execute the 'setup' function.
+    setup()
+  end
+end
+
 -- Alarm triggering the wifi_wait_ip function every 2500ms.
 tmr.alarm(1, 2500, 1, wifi_wait_ip)
--- relayr-API.lua
+++ relayr-API.lua
@@ -1,8 +1,10 @@
-local M  = {_VERSION = '0.1',
-    _NAME = 'relayrAPI',
-    _DESCRIPTION = 'relayr wrapper for the NodeMCU MQTT module',
-    _DEBUG = true,
-    _PORT = 1883,
+local M  = {
+  _VERSION = '0.1',
+  _NAME = 'relayrAPI',
+  _DESCRIPTION = 'relayr API wrapper for the NodeMCU MQTT module',
+  _DEBUG = false,
+  _PORT = 1883,
+  _SERVER = 'mqtt.relayr.io',
 }
 
 --- Some local definitions.
@@ -19,9 +21,9 @@ local format = string.format
 -- Avoid polluting the global environment.
 -- If we are in Lua 5.1 this function exists.
 if _G.setfenv then
-    setfenv(1, {})
+  setfenv(1, {})
 else -- Lua 5.2.
-    _ENV = nil
+  _ENV = nil
 end
 
 --- Subscribe to 'cmd' and 'config' MQTT topics.
@@ -29,17 +31,17 @@ end
 -- @return nothing
 --   Side effects only.
 local function subscribe_topics()
-    -- Subscribe to 'cmd' and 'config'.
-    client:subscribe({ [format('%scmd/', conf.topic)] = 0,
-        [format('%sconfig/', conf.topic)] = 0 },
-        function(conn)
-            -- Callback message on successful subscription.
-            if M._DEBUG then print('Succesfully subscribed to "cmd" and "config" topic') end
-        end)
+  -- Subscribe to 'cmd' and 'config'.
+  client:subscribe({ [format('%s/cmd/', conf.topic)] = 0,
+      [format('%s/config/', conf.topic)] = 0 },
+    function(conn)
+      -- Callback message on successful subscription.
+      if M._DEBUG then print('Succesfully subscribed to "cmd" and "config" topics.') end
+  end)
 end
 
 function M.register_data_listener(callbalck)
-    incoming_data = callbalck
+  incoming_data = callbalck
 end
 
 --- Register a callback for received data.
@@ -47,9 +49,9 @@ end
 -- @return nothing
 --   Side effects only.
 local function register_receive_callback()
-    client:on("message",
-        function(conn, topic, data)
-            if data then
+  client:on("message",
+            function(conn, topic, data)
+              if data then
                 -- Print the received data.
                 if M._DEBUG then print('Data recived: ' .. data) end
                 -- Decode the JSON data.
@@ -57,12 +59,12 @@ local function register_receive_callback()
                 -- Check if callback function is registered
                 -- in the main application.
                 if incoming_data then
-                    incoming_data(d)
+                  incoming_data(d)
                 else
-                    if M._DEBUG then print('Listener is not registered.') end
+                  if M._DEBUG then print('Listener is not registered.') end
                 end
-            end
-        end)
+              end
+  end)
 end
 
 --- Send data to relyr cloud.
@@ -72,16 +74,16 @@ end
 -- @return nothing
 --   Side effects only.
 function M.send(data)
-    -- Encode the data into JSON message and publish it.
-    local ok, json = pcall(cjson.encode, data)
-    if ok then
-        client:publish(format('%sdata/', conf.topic), json, 0, 0,
-            function(client)
-                if M._DEBUG then print('Sent: ' .. json) end
-            end)
-    else
-        if M._DEBUG then print('Failed to encode!') end
-    end
+  -- Encode the data into JSON message and publish it.
+  local ok, json = pcall(cjson.encode, data)
+  if ok then
+    client:publish(format('%s/data/', conf.topic), json, 0, 0,
+                   function(client)
+                     if M._DEBUG then print('Sent: ' .. json) end
+    end)
+  else
+    if M._DEBUG then print('Failed to encode!') end
+  end
 end
 
 --- Connect the client to the relayr cloud.
@@ -93,26 +95,26 @@ end
 -- @return nothing
 --   Side effects only.
 function M.connect(config, callback)
-    -- Save the config table locally.
-    conf = config
-    -- Load the
-    -- Create the MQTT client.
-    client = mqtt.Client(conf.clientID, 120, conf.user, conf.password)
-    -- Check if client was created.
-    if not client then print('not initialized') return end
-    -- Register callback for received data from subscribed topic.
-    register_receive_callback()
-    -- Connect to the relayr MQTT broker.
-    client:connect(conf.server, M._PORT, 0, 1, function(con)
-        print('Connection to the broker established.')
-        -- Subscribe to the '/cmd' topic.
-        subscribe_topics()
-        -- Trigger a callback when connection is established.
-        callback()
-    end, function(con, reason)
-        -- If the connection could not be established print the reason for it.
-        print(format('Connection to the broker could not be established. Reason: %s', reason))
-    end)
+  -- Save the config table locally.
+  conf = config
+  -- Load the
+  -- Create the MQTT client.
+  client = mqtt.Client(conf.clientID, 120, conf.user, conf.password)
+  -- Check if client was created.
+  if not client then print('not initialized') return end
+  -- Register callback for received data from subscribed topic.
+  register_receive_callback()
+  -- Connect to the relayr MQTT broker.
+  client:connect(conf.server, M._PORT, 0, 1, function(con)
+                   print('Connection to the broker established.')
+                   -- Subscribe to the '/cmd' topic.
+                   subscribe_topics()
+                   -- Trigger a callback when connection is established.
+                   callback()
+                                             end, function(con, reason)
+                   -- If the connection could not be established print the reason for it.
+                   print(format('Connection to the broker could not be established. Reason: %s', reason))
+  end)
 end
 
 return M
